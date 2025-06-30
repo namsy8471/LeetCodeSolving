1ce800960ee07902ca536d87bb7957e2e0fe23c4
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-06-30 16:43:37 +0900

    Time: 1 ms (22.89%), Space: 8.6 MB (50.78%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long left = 0;
+        long long right = x;
+
+        int max = 0;
+
+        while (left <= right)
+        {
+            long long mid = left + (right - left) / 2;
+            
+            if (mid * mid > x) right = mid - 1;
+            else
+            {
+                left = mid + 1;
+                max = mid;
+            }
+        }
+
+        return max;
     }
 };
