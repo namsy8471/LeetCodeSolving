9553060f3afccf2ba1da70abd5775c217caed7f7
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-07-01 13:13:19 +0900

    Time: 0 ms (100%), Space: 13 MB (14.88%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -17,34 +17,13 @@ public:
 
     bool checkRecursion(TreeNode* n1, TreeNode* n2)
     {
-        if (n1 == nullptr && n2 == nullptr) return true;
-        else if (n1 == nullptr || n2 == nullptr) return false;
+        if (n1 == nullptr && n2 == nullptr)
+            return true;
 
-        if (n1->val != n2->val) return false;
+        if (n1 == nullptr || n2 == nullptr || n1->val != n2->val)
+            return false;
 
-        bool ret1 = true;
-        bool ret2 = true;
-
-        if (n1->left == nullptr && n2->left == nullptr) ret1 = true;
-        else if (n1->left == nullptr || n2->left == nullptr) return false;
-        else
-        {
-            if (n1->left->val == n2->left->val) {
-                ret1 = checkRecursion(n1->left, n2->left);
-            }
-            else return false;
-        }
-
-        if (n1->right == nullptr && n2->right == nullptr) ret2 = true;
-        else if (n1->right == nullptr || n2->right == nullptr) return false;
-        else
-        {
-            if (n2->right->val == n2->right->val) {
-                ret2 = checkRecursion(n1->right, n2->right);
-            }
-            else return false;
-        }
-
-        return ret1 && ret2;
+        return checkRecursion(n1->left, n2->left) 
+            && checkRecursion(n1->right, n2->right);
     }
 };
