424a76b24dc773848ffdc863139e755cf5303dcd
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2026-04-15 15:41:37 +0900

    Time: 0 ms (100%), Space: 9 MB (66.47%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,8 +1,12 @@
 class Solution {
+    array<array<int, 21>, 21> memo;
 public:
     bool isMatch(string s, string p) {
         int i = 0;
         int j = 0;
+        
+        for(auto& row : memo)
+            row.fill(-1);
 
         return backTrack(i, j, s, p);
     }
@@ -12,11 +16,17 @@ public:
         if(j == p.length())
             return i == s.length();
         
+        if(memo[i][j] != -1) return memo[i][j];
+
         bool currentMatch = (i < s.length() && (s[i] == p[j] || p[j] == '.'));
+        bool ans;
 
         if(j + 1 < p.length() && p[j + 1] == '*')
-            return backTrack(i, j + 2, s, p) || (currentMatch && backTrack(i + 1, j, s, p));
+            ans = backTrack(i, j + 2, s, p) || (currentMatch && backTrack(i + 1, j, s, p));
         else
-            return  currentMatch && backTrack(i + 1, j + 1, s, p);
+            ans =  currentMatch && backTrack(i + 1, j + 1, s, p);
+
+        memo[i][j] = ans;
+        return memo[i][j];
     }
 };
