e615b1443148602fa5eb7fa4da0331057bf25d35
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2026-04-29 13:06:21 +0900

    Time: 20 ms (92.14%), Space: 70.9 MB (96.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,8 +1,13 @@
+// 함수 외부(클래스 밖 혹은 안의 static)에 두면 전역적으로 한 번만 실행됩니다.
+static const int speedup = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);
         
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size() - 1; i++)
