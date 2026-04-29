7c8bd86e1b9b092c73bd42275c9b0c5a12abfbe9
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2026-04-29 13:49:21 +0900

    Time: 0 ms (100%), Space: 40.1 MB (94.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> lMul(nums.size(), 1);
-        vector<int> rMul(nums.size(), 1);
+        vector<int> res(nums.size(), 1);
 
         for(int i = 1; i < nums.size(); i++)
         {
-            lMul[i] *= lMul[i - 1] * nums[i - 1]; 
+            res[i] *= res[i - 1] * nums[i - 1]; 
         }
 
-        for(int i = nums.size() - 2; i >= 0; i--)
+        int rightProduct = 1;
+        for(int i = nums.size() - 1; i >= 0; i--)
         {
-            rMul[i] *= rMul[i + 1] * nums[i + 1];
+            res[i] *= rightProduct;
+            rightProduct *= nums[i];
         }
 
-        for(int i = 0; i < nums.size(); i++)
-        {
-            nums[i] = lMul[i] * rMul[i];
-        }
-
-        return nums;
+        return res;
     }
 };
