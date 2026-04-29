c1079155fa951d4e16075729738e35c3b6dee38e
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2026-04-29 13:03:46 +0900

    Time: 18 ms (95.81%), Space: 70.9 MB (89.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
         
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size() - 1; i++)
